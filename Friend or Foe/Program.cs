30b08e6eb28c5ae33e4318cfd5a7f32ab56342cb
MarcinB <marcio_br@interia.pl>
2017-11-24 13:01:19 +0100

    Add project files.

---
+++ Friend or Foe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Friend_or_Foe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
